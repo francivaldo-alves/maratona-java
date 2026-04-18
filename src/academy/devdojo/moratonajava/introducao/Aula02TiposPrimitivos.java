0ca97315a006d95a0e0cae271823e93ede714f9a
Francivaldo <francivaldo.alves@gmail.com>
2026-04-18 11:00:59 -0300

    Aula 02 - Tipos primitivos

---
+++ src/academy/devdojo/moratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,26 @@
+package academy.devdojo.moratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+
+    public static void main(String[] args) {
+
+        // int, double, floot char, byte short, long boolean
+        int idade=10;
+        long numeroGrande = 1000000;
+        double SalarioDouble =2000;
+        float salarioFloat = 1200;
+        byte  idadeByte=10;
+        short idadeShort = 10;
+        boolean verdadeiro=true;
+        boolean falso=false;
+        char caracter=87;
+        String nome ="Moratona";
+        System.out.println(caracter);
+
+
+
+
+
+
+    }
+}
