b48a66ae6022eac9dcdac1919a177f96837b4265
Francivaldo <francivaldo.alves@gmail.com>
2026-04-18 11:25:59 -0300

    Aula 02 -Exercicio

---
+++ src/academy/devdojo/moratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,17 @@
+package academy.devdojo.moratonajava.introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+
+
+    public static void main(String[] args) {
+        String nome="Francivaldo";
+        String endereco ="Rua joão 23, 935";
+        double salario=3000.00D;
+        String data="17/04;2026";
+        String relatorio = "Eu " + nome + " Morando no endereço  " + endereco + "Confirmo que recebi o salário de " + salario + " R$ na data " + data;
+
+        System.out.println( relatorio);
+
+    }
+
+}
